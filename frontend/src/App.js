e1578d1c768c3dd5a90cc5f232a147d524d04972
jakubfalowski <kubafa99@gmail.com>
2022-10-26 16:18:51 +0200

    feat: router pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,28 @@ function App() {
     <Router>
         <Routes>
           <Route path={'/'} element={<Component />} />
+
+          <Route path={'/subjects'} element={<div></div>} />
+          <Route path={'/subject/:name'} element={<div></div>} />
+
+          <Route path={'/rooms'} element={<div></div>} />
+          <Route path={'/room/:name'} element={<div></div>} />
+
+          <Route path={'/departments'} element={<div></div>} />
+          <Route path={'/department/:name'} element={<div></div>} />
+
+          <Route path={'/login'} element={<div></div>} />
+          <Route path={'/register'} element={<div></div>} />
+
+          <Route path={'/myaccount'} element={<div></div>} />
+          <Route path={'/assigment/request'} element={<div></div>} />
+          <Route path={'/assigment/accept'} element={<div></div>} />
+
+          <Route path={'/edit-users'} element={<div></div>} />
+
+          <Route path={'*'} element={<div><p>Podana strona nie istnieje</p></div>} />
+
+
         </Routes>
       </Router>
 
