b1cd8dc3e1fa5e97ef558f7cc058fdc8dd55ce24
jakubfalowski <kubafa99@gmail.com>
2022-11-14 23:24:35 +0100

    fix: add address and phone data to department view

--- frontend/src/exampleData/IDepartmentData.js
+++ frontend/src/exampleData/IDepartmentData.js
@@ -3,18 +3,24 @@ export const IDepartmentData = {
         NAME: "Uniwersytet Rzeszowski",
         DESCRIPTION: "Urzet urzet na kolana wyruchamy wam dziekana",
         FIRM: "Rzeczpospolita Polska",
-        SHORT_NAME: "URZ"
+        SHORT_NAME: "URZ",
+        ADDRESS: "Rzeszów",
+        PHONE: "511555992"
     },
     1:{
         NAME: "Politechnika Rzeszowska",
         DESCRIPTION: "perzet perzet umyj zęby, wyruchamy wam studentki",
         FIRM: "Rzeczpospolita Polska",
-        SHORT_NAME: "PRZ"
+        SHORT_NAME: "PRZ",
+        ADDRESS: "Rzeszów",
+        PHONE: "554213422"
     },
     2:{
         NAME: "Wyższa Szkoła Informatyki i Zarządzania",
         DESCRIPTION: "Wyższa Szkoła Informatyki i Zarządzania, długa nazwa twoja stara",
         FIRM: "Rzeczpospolita Polska",
-        SHORT_NAME: "WSIiZ"
+        SHORT_NAME: "WSIiZ",
+        ADDRESS: "Rzeszów",
+        PHONE: "544245567"
     }
 }
--- frontend/src/views/DeparmentInfo.js
+++ frontend/src/views/DeparmentInfo.js
@@ -16,14 +16,12 @@ export default function DepartmentInfo(){
                                 <h2>{item.NAME}</h2>
                                 <h4>{item.DESCRIPTION}</h4>
                                 <Text weight={100}>Właściciel: {item.FIRM}</Text><br />
-                                <Text><IconHome2 size={20} /> Rzeszów</Text><br />
-                                <Text><IconPhoneCall size={20} /> 505606707</Text><br />
+                                <Text><IconHome2 size={20} />{item.ADDRESS}</Text><br />
+                                <Text><IconPhoneCall size={20} />{item.PHONE}</Text><br />
                             </>
                         )
                     }
                     else return null
-                        
-
                 })
             }
         </Container>
