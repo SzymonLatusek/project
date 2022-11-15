9639726733659eecc8bfcb5d3de9d4cb3f2b2d1e
jakubfalowski <kubafa99@gmail.com>
2022-11-15 14:37:28 +0100

    feat: list of rooms in departments

---
+++ frontend/src/exampleData/IRoomsData.js
@@ -0,0 +1,29 @@
+export const IRoomsData = {
+    0:{
+        NUMBER: '142',
+        TYPE: "wykładowa",
+        MAX_PERSON: '100',
+        WIDTH: '50',
+        LENGTH: '30',
+        HEIGHT: '5',
+        DESCRIPTION: "na wielu miejscach zniszczone ławki napisami JB"
+    },
+    1:{
+        NUMBER: '345',
+        TYPE: "ćwiczeniowa",
+        MAX_PERSON: '30',
+        WIDTH: '20',
+        LENGTH: '20',
+        HEIGHT: '3',
+        DESCRIPTION: "-"
+    },
+    2:{
+        NUMBER: '398',
+        TYPE: "laboratoryjnaa",
+        MAX_PERSON: '15',
+        WIDTH: '10',
+        LENGTH: '15',
+        HEIGHT: '3',
+        DESCRIPTION: "w rogu brzęcząca lodówka"
+    }
+}
--- frontend/src/views/DeparmentInfo.js
+++ frontend/src/views/DeparmentInfo.js
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom';
-import { Container, Text } from '@mantine/core';
+import { Container, Text, Accordion, Anchor } from '@mantine/core';
 import { IDepartmentData } from '../exampleData/IDepartmentData';
-import { IconHome2, IconPhoneCall } from '@tabler/icons';
+import { IRoomsData } from '../exampleData/IRoomsData';
+import { IconHome2, IconPhoneCall, IconPlus } from '@tabler/icons';
 
 export default function DepartmentInfo(){
     const { name } = useParams();
 
+    console.log(IRoomsData)
     return(
         <Container size="xs" px="xs" sx={{textAlign: "center"}}>
             {
@@ -24,7 +26,28 @@ export default function DepartmentInfo(){
                     else return null
                 })
             }
-        </Container>
+        <h2>Pomieszczenia</h2>
+        <Accordion variant="separated" radius="lg" chevronPosition="left" disableChevronRotation transitionDuration={1000} chevron={<IconPlus size={16} />}
+        sx={{
+            '[data-active="true"] svg': {
+              transform: 'rotate(45deg)',
+            },
+        }}>
+          {
+            Object.values(IRoomsData).map(item =>(
+                <Accordion.Item value={item.NUMBER}>
+                <Accordion.Control w sx={{textAlign: "center"}}>Sala nr. {item.NUMBER}, {item.MAX_PERSON} osobowa, {item.TYPE}</Accordion.Control>
+                <Accordion.Panel>{item.DESCRIPTION}<br />
+                    <Text weight={200}>Wymiary: (szerokość x długość x wysokość) {item.WIDTH} x {item.LENGTH} x {item.HEIGHT} </Text><br />
+                    <Anchor href={`/room/${item.NUMBER}`} target="_blank">
+                        Zobacz szczegóły
+                    </Anchor>
+                </Accordion.Panel>
+                </Accordion.Item>
+            ))
+          }
+      </Accordion>
+    </Container>
     )
 
 }
