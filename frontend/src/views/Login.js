e96c8ba045e6a94ae09aa36332f2f93ada652371
SzymonLatusek <58951643+SzymonLatusek@users.noreply.github.com>
2022-11-16 01:57:42 +0100

    Update Login.js
    Widok Logowania

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -1,6 +1,37 @@
+import { PasswordInput, TextInput,Button } from '@mantine/core';
+
+
+import '../styles/App.scss';
 export default function Login(){
     return(
-        <div>
+<div className='container'>
+        <div className='inputContainer'>
+        <TextInput
+        placeholder=" Wpisz Login"
+        label="Login"
+        withAsterisknpm
+        />
+         <PasswordInput
+        placeholder="Wpisz Hasło"
+        label="Hasło"
+        withAsterisknpm
+        />
+        <Button type="submit" fullWidth variant="gradient" gradient={{ from: 'dark', to: 'black', deg: 200 }}>
+                        Zaloguj
+                    </Button>
+
+
         </div>
+        
+        </div>
+        
+        
+        
+ 
+    
+
     )
+    
+
+
 }
